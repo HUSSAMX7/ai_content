00779626c3bf8c29d71bd8b3fff8076c941cd631
HUSSAMX7 <hosam_z9@example.com>
2026-05-12 14:26:44 +0300

    Refactor save_output function to use a dedicated output directory and dynamic file path for saved results.

--- nodes/save_output.py
+++ nodes/save_output.py
@@ -1,5 +1,10 @@
+import os
+from pathlib import Path
 from graph_state import GraphState
 from langgraph.types import interrupt
+
+OUTPUT_DIR = Path("generated_file")
+
 
 def save_output(state: GraphState) -> dict:
 
@@ -7,12 +12,13 @@ def save_output(state: GraphState) -> dict:
 
     file_name = interrupt("ادخل اسم الملف الذي تريد حفظ النتيجة فيه ؟")
     
+    OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
-    with open(f"{file_name}.md", "w", encoding="utf-8") as f:
+    sotred_file_name = OUTPUT_DIR / f"{file_name}.md"
+
+    with open(sotred_file_name, "w", encoding="utf-8") as f:
         f.write(final_content)
 
-    print(f"\nSaved output to: {file_name}.md")
+    print(f"\nSaved output to: {sotred_file_name}")
     return {}
-
-
 
